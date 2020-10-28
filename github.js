7a22f555659c8f02b77cfe98b7c26d03101fa5ad
Your Name <you@example.com>
2020-10-28 11:36:50 -0400

    [github] add obj

--- github.js
+++ github.js
@@ -1,9 +1,24 @@
+const container = document.querySelector('.container')
+
+
 fetch('https://api.github.com/users/CoreyWilson319')
 .then(function(response){
     return response.json();
 })
 .then(function(githubData){
-    console.log(githubData);
+    const coreyObj = {
+        bio: githubData.bio,
+        username: githubData.login,
+        name: githubData.name
+    }
+    console.log(coreyObj);
+
+    const newElement = document.createElement('p');
+    newElement.textContent = coreyObj.name;
+    // append newElement to container(parent)
+    container.appendChild(newElement);
+
+    // console.log(githubData);
 })
 
 .catch(function(error) {
